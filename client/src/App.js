216708629b76e9d8b7d663c8307e51dbc079fe8e
unknown <volkan.goksu@outlook.com>
2021-07-31 06:18:03 +0300

    calculate component forms

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import Calculate from "./components/Calculate";
 import Result from "./components/Result";
 function App() {
   return (
-    <div className="App flex justify-center pt-4 pb-">
+    <div className="App flex justify-center pt-4 pb-2 border-solid border-4 rounded">
        <div className="app-container w-3/4 min-h-full">
           <Header />
           <div className="content flex w-full h-full">
--- client/src/components/Calculate.js
+++ client/src/components/Calculate.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 export default function Calculate() {
-    return (
-        <div className="calculate w-1/2 ">
-            <h1>Calculate</h1>
+  return (
+    <div className="calculate w-1/2 ">
+      <form>
+        <div className="form">
+          <p>Credit Amount</p>
+          <div className="input-group flex  items-center ">
+            <input type="number" />
+          </div>
         </div>
-    )
+        <div className="form">
+          <p>Pay instalment</p>
+          <div className="input-group">
+            <input type="number" />
+          </div>
+        </div>
+        <div className="form">
+          <p>Bank Rate</p>
+          <div className="input-group">
+            <input type="number" />
+          </div>
+        </div>
+        <div className="button-container bg-gray-150">
+          <button type="submit bg-gray-900">Calculate</button>
+        </div>
+      </form>
+    </div>
+  )
 }
